2f944c6007661fb48d4c2572cfdf57ea0189cf14
Elena Vigo <elenavigoolivan@gmail.com>
2020-05-05 23:03:03 +0200

    javascript title responsive and redirection to projects

--- app/javascript/packs/scroll_banner_info.js
+++ app/javascript/packs/scroll_banner_info.js
@@ -1,43 +1,51 @@
 const scrolling = () => {
-  const lastKnownScollY = 0;
-  const currentScrollY = 0;
-  const ticking = false;
-  const eleHeader = null;
+  let lastKnownScrollY = 0;
+  let currentScrollY = 0;
+  let ticking = false;
+  const idOfHeader = 'banner-info';
+  let eleHeader = null;
   const classes = {
     pinned: 'header-pin',
-    unpinned: 'header-unpin'
-  }
-
+    unpinned: 'header-unpin',
+  };
   const onScroll = () => {
     currentScrollY = window.pageYOffset;
-    if (currentScrollY < lastKnownScollY) {
-      pin()
-    } else if (currentScrollY > lastKnownScollY) {
-      unpin()
+    requestTick();
+  };
+  const requestTick = () => {
+    if (!ticking) {
+      requestAnimationFrame(update);
     }
-    lastKnownScollY = currentScrollY;
-  }
-
+    ticking = true;
+  };
+  const update = () => {
+    if (currentScrollY < lastKnownScrollY) {
+      pin();
+    } else if (currentScrollY > lastKnownScrollY) {
+      unpin();
+    }
+    lastKnownScrollY = currentScrollY;
+    ticking = false;
+  };
   const pin = () => {
     if (eleHeader.classList.contains(classes.unpinned)) {
       eleHeader.classList.remove(classes.unpinned);
       eleHeader.classList.add(classes.pinned);
     }
-  }
-
+  };
   const unpin = () => {
     if (
-      eleHeader.classList.contains(classes.pinned) ||
-      !eleHeader.classList.contains(classes.unpinned)
+      eleHeader.classList.contains(classes.pinned)
+      || !eleHeader.classList.contains(classes.unpinned)
     ) {
       eleHeader.classList.remove(classes.pinned);
       eleHeader.classList.add(classes.unpinned);
     }
-  }
-  Window.onload = () => {
-    eleHeader = document.getElementById('banner-info');
-    document.addEventListener('scroll', onScroll, false)
-  }
-}
+  };
+  window.onload = () => {
+    eleHeader = document.getElementById(idOfHeader);
+    window.addEventListener('scroll', onScroll, false);
+  };
+};
 
 export default scrolling;
