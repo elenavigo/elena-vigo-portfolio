187f0d525c2b28c9fc2addbfd0da8f290243b42c
Elena Vigo <elenavigoolivan@gmail.com>
2020-05-17 11:55:14 +0200

    changes in projects and links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
 // import '../../assets/stylesheets/application.css.scss';
 import 'bootstrap';
 import AOS from 'aos';
-import scrolling from './scroll_banner_info';
+// import scrolling from './scroll_banner_info';
 import scrollTop from './scroll_top';
 
 AOS.init();
 
-scrolling();
+// scrolling();
 
 
 scrollTop();
